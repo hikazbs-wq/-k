1a81e5108e00087e0598ab8e7451229bf012dd5f
hikazbs-wq <hikazbs@gmail.com>
2026-03-13 19:02:09 +0300

    Create data.js with vehicle brands and models data.

---
+++ data.js
@@ -0,0 +1,26 @@
+const vehicleData = {
+  brands: [
+    {
+      name: 'Toyota',
+      models: ['Camry', 'Corolla', 'RAV4']
+    },
+    {
+      name: 'Honda',
+      models: ['Civic', 'Accord', 'CR-V']
+    },
+    {
+      name: 'Ford',
+      models: ['F-150', 'Mustang', 'Explorer']
+    },
+    {
+      name: 'Chevrolet',
+      models: ['Silverado', 'Malibu', 'Equinox']
+    },
+    {
+      name: 'Nissan',
+      models: ['Altima', 'Sentra', 'Rogue']
+    }
+  ]
+};
+
+module.exports = vehicleData;
